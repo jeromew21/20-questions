40213b95d9295269aafc423426a73e5c1bace7cd
Jerome Wei <jeromew@berkeley.edu>
2018-11-04 01:00:01 -0800

    made some changes

--- bruteforce20questions.py
+++ bruteforce20questions.py
@@ -31,21 +31,24 @@ def diff(arr1, arr2):
     return sum(d*d)
 
 def ordered_by_diff(pairs, input_arr):
-    triples = [(label, arr, diff(arr, input_arr)) for label, arr in pairs]
+    #Mask each pair to only questions answered in input_arr
+    masked_pairs = [(label, np.copy(p)) for label, p in pairs]
+    for pair in masked_pairs:
+        for i in range(len(input_arr)):
+            if input_arr[i] == 0:
+                pair[1][i] = 0
+    triples = [(label, arr, diff(arr, input_arr)) for label, arr in masked_pairs]
     return [(k[0], k[2]) for k in sorted(triples, key=lambda t: t[2])]
 
 def get_input_arr():
     result = np.array([0 for i in questions])
     diffs = ordered_by_diff(data, result)
-    print(diffs)
 
     print(f"There are {len(questions)} questions.")
     for index, question in enumerate(questions):
         ans = ask(question)
         result[index] = lookup.get(ans, -1)
         diffs = ordered_by_diff(data, result)
-        print("Top:")
-        print(diffs)
         print(f"Current guess: {diffs[0][0]}")
         
 get_input_arr()
